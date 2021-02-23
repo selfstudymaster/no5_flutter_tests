638ab26bb5b6c5bd8ada489746449ff49761842e
selfstudymaster <webselfmaster@gmail.com>
2021-02-23 15:54:08 +0900

    コメント追加

--- lib/controller/connpass_api.dart
+++ lib/controller/connpass_api.dart
@@ -1,13 +1,10 @@
 import 'package:http/http.dart' as http;
 
-
-
-class ConpassAPI{
-  Future<String> fetchPosts(String searchWord ) async {
-    http.Response response = await http.get('https://connpass.com/api/v1/event/?count=100&order=1&keyword=$searchWord');
+class ConpassAPI {
+  Future<String> fetchPosts(String searchWord) async {
+    http.Response response = await http.get(
+        'https://connpass.com/api/v1/event/?count=100&order=1&keyword=$searchWord');
     // http.Response response = await http.get('https://connpass.com/api/v1/event/?count=100&order=1&keyword=$searchWord');
-      return response.body;
-
+    return response.body;
   }
-
 }
--- lib/model/conpass_api_model_events.dart
+++ lib/model/conpass_api_model_events.dart
@@ -1,20 +1,19 @@
-class ConpassAPIModelEvents{
-var title;
-var description;
-var event_url;
-var Catch;
+class ConpassAPIModelEvents {
+  var title;
+  var description;
+  var event_url;
+  var Catch;
 
-ConpassAPIModelEvents(
+  ConpassAPIModelEvents(
     this.title,
     this.description,
     this.event_url,
     this.Catch,
-    );
+  );
 
-
-ConpassAPIModelEvents.fromJson(Map<String, dynamic> json)
+  ConpassAPIModelEvents.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         description = json['description'],
         event_url = json['event_url'],
-        Catch =  json['catch'];
+        Catch = json['catch'];
 }
